3dec2f1dd70ab5f560d8f6897332d1dc5b0b0712
eadehemingway <eade.hemingway@gmail.com>
2019-01-20 13:27:18 +0000

    its all working well, about to start messing with stuff

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,10 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/index.html',
-  filename: 'index.html'
+  filename: '../index.html'
 });
 module.exports = {
   entry: __dirname + '/public/index.tsx',
+  output: {
+    path: __dirname + '/dist/public',
+    filename: 'index.bundle.js'
+  },
   module: {
     rules: [
       {
@@ -15,12 +19,7 @@ module.exports = {
       }
     ]
   },
-
   mode: 'development',
-  output: {
-    path: __dirname + '/dist/public',
-    filename: 'index.bundle.js'
-  },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.css', '.scss']
   },
