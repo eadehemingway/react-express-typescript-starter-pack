df0a5eb57b477ee8f09e77952d5502b2dc3d3f06
eadehemingway <eade.hemingway@gmail.com>
2019-02-09 14:21:12 +0000

    add comments

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// we only need to do this config of the HTMLplugin because we have our own template html file.
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/public/index.html',
-  filename: 'index.html'
+  template: __dirname + '/public/index.html'
 });
+
 module.exports = {
+  // __dirname refers to the location of the current file, then it says the entry point is the index.tsx file
+  // the entry point is this file because this is the one typescript file on the front end where every other file is loaded into
   entry: __dirname + '/public/index.tsx',
   output: {
     path: __dirname + '/dist/public',
+    // the filename :'index.bundle.js' will be the name of the file that all the compiled code gets spat into,
+    // it will also be the name that the script tag links to in the index.html file in the dist folder
     filename: 'index.bundle.js'
   },
   module: {
